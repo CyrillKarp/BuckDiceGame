1d963b3ef849556df5510bf2601d93ef230007cb
Cyrill <torreskirill@yandex.ru>
2018-03-13 01:40:40 +0300

    add js

--- js/script.js
+++ js/script.js
@@ -1,69 +1,47 @@
-let scores, activePlayer, gamePlaying;
-
+let scores, activePlayer, gamePlaying, randomPoint, randomDice;
 
 init();
-
-// 1. Get a random number from 1 to 6
-let randomDice = () => Math.floor(Math.random() * 6) + 1;
-
-//2. Display an point (ace)
-let randomPoint = randomDice();
-//console.log(randomPoint);
-let dicePoint = document.getElementById('dice-point');
-dicePoint.src = 'img/dice-' + randomPoint + '.png';
 
 //3. Get player's dices
 function getPlayersDice() {
     let dices = [];
     for (let i = 0; i < 3; i++) {
         dices.push(randomDice());
-
-    };
+    }
     return dices;
 };
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
-    //4. Display player's dices
-    let diceDOM = getPlayersDice();
-    //console.log(diceDOM);
-    for (let i = 0; i < 3; i++) {
-        document.getElementById('dice-' + i).src = 'img/dice-' + diceDOM[i] + '.png';
-    };
+    if (gamePlaying) {
+        //4. Display player's dices
+        let diceDOM = getPlayersDice();
+        //console.log(diceDOM);diceDOM.style.display = 'block';
+        document.querySelector('.dice-img').style.display = 'block';
+        for (let i = 0; i < 3; i++) {
+            document.getElementById('dice-' + i).src = 'img/dice-' + diceDOM[i] + '.png';
+        };
 
-    //5. Check the coincidence of numbers
-    //    for (let i = 0; i < 3; i++) {
-    //        if (randomPoint == diceDOM[i]) {
-    //            scores[activePlayer]++;
-    //            document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
-    //
-    //        }else {
-    //            //Next player
-    //           // nextPlayer();
-    //            console.log('her');
-    //        }
-    //    }
+        //5. Check the coincidence of numbers
+        function isRandomPoint(number) {
+            return number === randomPoint;
+        };
 
-    let strDice = '';
-
-    for (let i in diceDOM) {
-        /* если есть совпадения, то помещаем 
-         * индекс (i) элемента в результирующую строку */
-        if (diceDOM[i] == randomPoint) {
+        if (diceDOM.some(isRandomPoint)) {
             scores[activePlayer]++;
             document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
-        }else {
-                //Next player
-                nextPlayer();
-                //console.log('her');
+            if (scores[activePlayer] >= 15) {
+                document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+                document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+                //document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+                gamePlaying = false;
             }
+
+        } else {
+            //Next player
+            nextPlayer();
+        }
     }
-    
-
 });
-
-
-
-
 
 function nextPlayer() {
     //Next player
@@ -71,22 +49,32 @@ function nextPlayer() {
 
     document.querySelector('.player-0-panel').classList.toggle('active');
     document.querySelector('.player-1-panel').classList.toggle('active');
-
-    //    document.querySelector('.dice-section-0').style.display = 'none';
-    //    document.querySelector('.dice-section-1').style.display = 'none';
 }
-
-
 
 document.querySelector('.btn-new').addEventListener('click', init);
 
 function init() {
+    // 1. Get a random number from 1 to 6
+    randomDice = () => Math.floor(Math.random() * 6) + 1;
+
+    //2. Display an point (ace)
+    randomPoint = randomDice();
+    //console.log(randomPoint);
+    let dicePoint = document.getElementById('dice-point');
+    dicePoint.src = 'img/dice-' + randomPoint + '.png';
+
     scores = [0, 0];
     activePlayer = 0;
     gamePlaying = true;
 
+    document.querySelector('.dice-img').style.display = 'none';
+
     document.getElementById('score-0').textContent = '0';
     document.getElementById('score-1').textContent = '0';
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
     document.querySelector('.player-0-panel').classList.remove('active');
     document.querySelector('.player-1-panel').classList.remove('active');
     document.querySelector('.player-0-panel').classList.add('active');
